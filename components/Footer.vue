90558ba2bb42264c267456a14a801d7a72c465d1
Hector del Angel <hectordelangel@gmail.com>
2020-09-09 15:47:20 -0700

    Style the footer, add social icons

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="bg-black bg-opacity-75 text-white">
-    Footer
+  <div class="flex justify-between bg-chocolate h-full text-white">
+    <div class="p-4">
+      The Menjour Galerie &copy; 2020
+    </div>
+    <div class="flex justify-around p-4 mr-5">
+
+       <div class="text-lg">
+        <a href="" class="hover:text-lightbrown">
+
+          <fa :icon="['fab', 'instagram']" class="mr-2" />
+        </a>
+        <a href="" class="hover:text-lightbrown">
+          <fa :icon="['fab', 'twitter']" class="mr-2" />
+        </a>
+        <a href="" class="hover:text-lightbrown">
+          <fa :icon="['fab', 'etsy']" class="mr-2" />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
