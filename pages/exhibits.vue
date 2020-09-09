3a62b1d278662d742d0fc717c89012865266ec00
Hector del Angel <hectordelangel@gmail.com>
2020-09-09 16:57:31 -0700

    Style the content on exhibits page

--- pages/exhibits.vue
+++ pages/exhibits.vue
@@ -1,33 +1,52 @@
 <template>
   <div>
-    <div class="sm:flex sm:justify-between  bg-blue-200 bg-opacity-50 border rounded">
-      <!-- <div class="sm:ml-8 sm:mr-2 sm:p-8 sm:w-1/2"> -->
-      <div>
-        <div class="bg-cover shadow-xl">
-          <img src="../assets/img/surreal2.jpg" alt="" class="w-75 h-75 p-8">
-        </div>
-        <div class="mt-8 text-center">
-          <h1>Surreal</h1>
-          <hr>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt, veniam!</p>
+    <div class="my-4 mx-auto px-4 md:px-8">
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <!--1st Column -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <!-- Article -->
+              <section class="overflow-hidden rounded-lg shadow-lg">
+                <a href="#">
+                  <img alt="Surreal" class="block h-64 w-full rounded-lg shadow-xl" src="../assets/img/surreal 1.jpg">
+                </a>
+                <header class="bg-lightbrown opacity-75 p-2 md:p-4">
+                  <h1 class="text-2xl text-center">
+                      <a class="no-underline font-heading tracking-wide hover:text-lightbrown hover:bg-chocolate text-black text-center" href="#">
+                          Surreal Galerie
+                      </a>
+                  </h1>
+                  <p class="text-center font-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, accusamus.
+                  </p>
+                </header>
+              </section>
+            </div>
+            <!-- End 1st Column -->
+
+            <!-- 2nd Column -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <!-- Article -->
+              <section class="overflow-hidden rounded-lg shadow-lg">
+                <a href="#">
+                  <img alt="abstract" class="block h-64 w-full rounded-lg shadow-xl" src="../assets/img/abstract1.jpg">
+                </a>
+                <header class="bg-lightbrown opacity-75 p-2 md:p-4">
+                  <h1 class="text-2xl text-center">
+                      <a class="no-underline font-heading tracking-wide hover:text-lightbrown hover:bg-chocolate text-black text-center" href="#">
+                          Abstract Galerie
+                      </a>
+                  </h1>
+                  <p class="text-center font-body">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, illo quasi.
+                  </p>
+                </header>
+              </section>
+            </div>
+            <!-- END 2nd Column -->
         </div>
       </div>
-
-      <!-- <div class="sm:mr-8 mt-4 sm:mt-4 sm:ml-2 sm:p-8 sm:w-1/2 ">
-        <div class=" bg-blue-100 shadow-xl">
-          <img src="../assets/img/abstract2.jpg" alt="" class="p-8 object-cover">
-        </div>
-          <div class="mt-8 text-center">
-            <h1>Abstract</h1>
-            <hr>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          </div>
-      </div> -->
     </div>
   </div>
-  <!-- <div class="bg-blue-200">
-
-  </div> -->
 </template>
 
 <script>
