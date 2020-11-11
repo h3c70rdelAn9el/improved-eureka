ba9ddd829c69be71239197aef70395b2d582ef2d
Hector del Angel <hectordelangel@gmail.com>
2020-11-11 14:48:43 -0800

    Add and style contact page.

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    Contact
-  </div>
+<div class="p-2">
+  <h1 class="text-center text-3xl font-title">Contact Us</h1>
+  <hr class="border border-black shadow-lg">
+  <section class="bg-gray-300 mt-2 opacity-75 p-2 rounded-md">
+    <div class="p-2">
+      <p class="my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptate!</p>
+      <hr>
+      <p class="my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fugiat eaque dicta alias voluptate harum dolorem ducimus beatae minus quasi!</p>
+      <hr>
+      <p class="my-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi iure officia tempora, quo reprehenderit dicta doloremque laudantium autem.</p>
+    </div>
+    <div class="flex justify-center p-2">
+      <button class="border border-black bg-gray-800 text-gray-100 rounded p-1 hover:bg-gray-500 hover:text-gray-900">Contact Us</button>
+    </div>
+  </section>
+</div>
 </template>
