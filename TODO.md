e56bf6768666b2d07d86f6506c186f2a5ee0dbca
Hector del Angel <hectordelangel@gmail.com>
2020-11-11 14:24:37 -0800

    Deleting unnecessary commnets

--- TODO.md
+++
@@ -1,5 +0,0 @@
-11/3/20
-
-TODO:
-[] add tags to the exhibits/pages
-[] deploy
--- components/ArtPreview.vue
+++ components/ArtPreview.vue
@@ -2,15 +2,7 @@
 <div class="border border-black shadow-xl">
   <nuxt-link :to="id">
     <article class="rounded border border-black shadow">
-      <!-- <div
-          class="w-full"
-          :style="{background: `url(${artImage})`, backgroundSize: 'cover', height: 200 + 'px', width: 100% + 'px' }"
-        /> -->
       <div class="w-full rounded" :style="{backgroundImage: 'url(' + artImage + ')', backgroundSize: 'cover', height: 200 + 'px', width: 100% + 'px' }"></div>
-      <!-- <div
-      class="w-full"
-      :style="{background: `url(${artImage})`, backgroundSize: 'cover', height: 200 + 'px', width: 100% + 'px' }"
-    /> -->
       <div>
         <h1 class="text-center text-2xl font-title p-1 text-gray-100 bg-gray-800">{{ title }}</h1>
       </div>
