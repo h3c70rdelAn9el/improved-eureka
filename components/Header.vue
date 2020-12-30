bae41e8f0caff822b2e9d211ea006fbcb47dbe7e
Hector del Angel <hectordelangel@gmail.com>
2020-12-29 23:29:11 -0800

    Fix navbar links

--- components/Header.vue
+++ components/Header.vue
@@ -5,16 +5,41 @@
       <img class="h-12" src="../assets/img/logo.png" alt="logo">
     </nuxt-link>
   </div>
-  <div class="flex text-white pt-1 mb-0">
-    <nuxt-link to="/exhibits" class="rounded p-1 mb-4 text-lg no-underline hover:text-chocolate ml-2">
-      Exhibits
+  <div class="flex text-white pt-1 mb-0" v-for="link in links" :key="link.id">
+    <nuxt-link :to="`${link.route}`" class="rounded p-1 h-13 mb-4 text-lg no-underline hover:text-chocolate ml-2">
+      {{ link.name }}
     </nuxt-link>
+    <!--
     <nuxt-link to="/comingsoon" class="rounded p-1 mb-4 h-13 text-lg no-underline hover:text-chocolate ml-2">
       Coming Soon
     </nuxt-link>
     <nuxt-link to="/contact" class="rounded p-1 mb-4 text-lg no-underline hover:text-chocolate ml-2">
       Contact
     </nuxt-link>
+    -->
   </div>
 </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: 'Exhibits',
+          route: "/exhibits"
+        },
+        {
+          name: 'Coming Soon',
+          route: "/comingsoon"
+        },
+        {
+          name: 'Contact',
+          route: "/contact"
+        }
+      ]
+    }
+  },
+}
+</script>
